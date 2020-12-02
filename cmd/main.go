f8934a0d0cfc13c75f922710698e48aa735b6b19
Karl Casserfelt <karl.casserfelt@axis.com>
2020-12-02 19:26:41 +0100

    add timer to main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "karlc/aoc2020/src/solutions"
+import (
+	"fmt"
+	"karlc/aoc2020/src/solutions"
+	"time"
+)
 
 func main() {
+	start := time.Now()
 	solutions.Day1Part2()
+	fmt.Printf("Elapsed time: %v", time.Since(start))
 }
